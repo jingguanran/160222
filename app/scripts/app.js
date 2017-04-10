3d7b99e6c36e27bfe522ccb5a7baa97afd0f5f9a
jingguanran <jingguanran@126.com>
2017-04-10 14:56:19 +0800

    1111

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name 1602App
+ * @description
+ * # 1602App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('1602App', [
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 1602App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the 1602App
+ */
+angular.module('1602App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
